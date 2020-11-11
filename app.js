ca9c8708c356a9e326ca1cdf6fd9b2803ac975aa
JayLee1010 <73064612+JayLee1010@users.noreply.github.com>
2020-11-11 17:02:23 +0900

    1-6 Router part2
    Model data
    View 어떻게 보이는가
    Controller 데이터를 전사하는 방식
    View > 템플릿, 라우터

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from Home");
-
-const handleProfile = (req, res) => res.send("This is profile page");
 
 app.use(helmet());
 app.use(morgan("dev"));
@@ -16,10 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter); // "/user"로 들어가면 userRouter의 밑으로 들어가게됨
+app.use("/user", videoRouter);
 
 export default app;
--- router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-export const userRouter = express.Router();
-
-userRouter.get("/", (req, res) => res.send("Hi"));
-userRouter.get("/edit", (req, res) => res.send("edit page"));
-userRouter.get("/Password", (req, res) => res.send("Password page"));
