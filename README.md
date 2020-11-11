747bf673acfdda3b512f0fde77e7a2c8370eab0a
JayLee1010 <73064612+JayLee1010@users.noreply.github.com>
2020-11-11 16:25:36 +0900

    1-2 Middleware, arrowfunc

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 1. Install NodeJS >> 환경변수 고려
 2. WeTube 폴더 생성 후 VSC에 띄우기
 3. NPM init > json 폴더 있는 곳에서 npm 실행
+4. Nodejs의 .ignore 추가
+5. npm install @babel/node, preset-env, core
+6. npm install nodemon -D>> 저장할때마다 npm start 자동으로
+7. Middleware 추가해보기, next()로 퍼미션을 줘야 다음 단계로 넘어감 app.get("/", betweenHome, handlehome);
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hi")
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handlehome = (req, res) => res.send("Hello from Home");
+
+const handleprofile = (req, res) => res.send("This is profile page");
+
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+
+app.listen(PORT, handleListening);
+
+app.get("/", handlehome);
+
+app.get("/profile", handleprofile);
